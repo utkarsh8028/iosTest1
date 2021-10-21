060b728104c4d0e49dfafc5f67ddf1964fa37b5a
utkarsh <utkarsh8028@gmail.com>
2021-10-21 10:17:13 +0100

    Initial Commit

---
+++ gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by Sharma, Utkarsh on 21/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
